9509aab11f9133d636cc69f5f86ae3b2ad8e3040
Rebun <55385643+Rebun13@users.noreply.github.com>
2024-11-01 19:44:39 +0100

    Added components variable to game object

--- src/GameObjects/GameObject.h
+++ src/GameObjects/GameObject.h
@@ -1,9 +1,12 @@
+#include <vector>
+
 /**
  * (Abstract) Class for every object in the game environment.
  */
 class GameObject {
 private:
     const char* id;
+    const std::vector<GameObject> components;
 public:
     virtual const void render();
     virtual void tick();
